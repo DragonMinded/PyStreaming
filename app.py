fb6502758acb9ca6bceb10033c57dd97ec54c5e4
Jennifer Taylor <dragonminded@dragonminded.com>
2024-02-24 01:08:00 +0000

    Bump cache busting.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from data import Data
 
 
 # Allow cache-busting of entire frontend for stream page and chat updates.
-FRONTEND_CACHE_BUST: str = "site.1.0.3"
+FRONTEND_CACHE_BUST: str = "site.1.0.4"
 
 
 # Pictochat width/height, shared in a couple places.
